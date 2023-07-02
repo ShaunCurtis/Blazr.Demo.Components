777913e711db4632ad405e151e11dc36df0df7f2
Shaun Curtis <shaun@coldelm.com>
2023-07-02 09:03:22 +0100

    Update

---
+++ src/Blazr.Components/Services/ComponentServiceHandle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazr.Components.Services
+{
+    public class ComponentServiceHandle
+    {
+        private readonly ComponentServiceProvider _componentServiceProvider;
+        
+        public ComponentServiceHandle(ComponentServiceProvider componentServiceProvider) 
+        {
+            _componentServiceProvider = componentServiceProvider;
+        }
+
+        public TService? GetService<TService>(Guid serviceKey)
+           where TService : class
+           => _componentServiceProvider.GetOrCreateService<TService>(serviceKey);
+    }
+}
---
+++ src/Blazr.Components/Usings.cs
@@ -0,0 +1,4 @@
+﻿global using Microsoft.Extensions.DependencyInjection;
+global using System.Diagnostics;
+global using System.Diagnostics.CodeAnalysis;
+
